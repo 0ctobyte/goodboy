2220037aae51a729d964082e4c19ee043af9ad35
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2019-03-12 10:53:31 -0700

    Convert vectors to std::array in gb_memory_map and create alias template as well as convert _add_device_to_map to a template method

--- include/gb_memory_map.h
+++ include/gb_memory_map.h
@@ -2,12 +2,22 @@
 #define GB_MEMORY_MAP_H_
 
 #include <cstdint>
-#include <vector>
+#include <array>
 
 #include "gb_memory_mapped_device.h"
 
 #define GB_MEMORY_MAP_IO_BASE           (0xFF00)
 #define GB_MEMORY_MAP_SIZE              (0x10000)
+
+#define GB_MEMORY_MAP_WKRAM_START       (0xC000)
+#define GB_MEMORY_MAP_EORAM_START       (0xE000)
+#define GB_MEMORY_MAP_LOMEM_SIZE        (GB_MEMORY_MAP_EORAM_START)
+#define GB_MEMORY_MAP_LOMEM_BUCKET_SIZE (0x800)
+#define GB_MEMORY_MAP_LOMEM_NUM_BUCKETS (GB_MEMORY_MAP_LOMEM_SIZE/GB_MEMORY_MAP_LOMEM_BUCKET_SIZE)
+#define GB_MEMORY_MAP_HIMEM_START       (0xFE00)
+#define GB_MEMORY_MAP_HIMEM_SIZE        (0x200)
+#define GB_MEMORY_MAP_HIMEM_BUCKET_SIZE (0x1)
+#define GB_MEMORY_MAP_HIMEM_NUM_BUCKETS (GB_MEMORY_MAP_HIMEM_SIZE/GB_MEMORY_MAP_HIMEM_BUCKET_SIZE)
 
 class gb_memory_map {
 public:
@@ -20,7 +30,8 @@ public:
     void write_byte(uint16_t addr, uint8_t val);
 
 private:
-    using gb_device_map_t = std::vector<gb_memory_mapped_device_ptr>;
+   template <size_t S>
+    using gb_device_map_t = std::array<gb_memory_mapped_device_ptr, S>;
 
     // Memory is split into LOMEM (0000 - E000) and HIMEM (FE00 - 10000)
     // This way we can split up the granularity of region sizes for each hash table
@@ -28,12 +39,13 @@ private:
     // HIMEM hash tables have a granularity of 1 byte (for IO registers etc.)
     // The idea is that each device maps directly and completely (i.e no holes) into one or more entries in one or both tables
     // Also, we split tables by readable and writeable devices (i.e. ROM vs RAM or RO registers)
-    gb_device_map_t m_lomem_readable_devices;
-    gb_device_map_t m_lomem_writeable_devices;
-    gb_device_map_t m_himem_readable_devices;
-    gb_device_map_t m_himem_writeable_devices;
+    gb_device_map_t<GB_MEMORY_MAP_LOMEM_NUM_BUCKETS> m_lomem_readable_devices;
+    gb_device_map_t<GB_MEMORY_MAP_LOMEM_NUM_BUCKETS> m_lomem_writeable_devices;
+    gb_device_map_t<GB_MEMORY_MAP_HIMEM_NUM_BUCKETS> m_himem_readable_devices;
+    gb_device_map_t<GB_MEMORY_MAP_HIMEM_NUM_BUCKETS> m_himem_writeable_devices;
 
-    void _add_device_to_map(gb_device_map_t& device_map, const gb_memory_mapped_device_ptr& device, uint16_t start_addr, size_t size, size_t bucket_size);
+    template <size_t S>
+    void _add_device_to_map(gb_device_map_t<S>& device_map, const gb_memory_mapped_device_ptr& device, uint16_t start_addr, size_t size, size_t bucket_size);
 };
 
 #endif // GB_MEMORY_MAP_H_
--- src/gb_memory_map.cc
+++ src/gb_memory_map.cc
@@ -4,28 +4,16 @@
 #include "gb_logger.h"
 #include "gb_memory_map.h"
 
-#define GB_MEMORY_MAP_WKRAM_START       (0xC000)
-#define GB_MEMORY_MAP_EORAM_START       (0xE000)
-#define GB_MEMORY_MAP_LOMEM_SIZE        (GB_MEMORY_MAP_EORAM_START)
-#define GB_MEMORY_MAP_LOMEM_BUCKET_SIZE (0x800)
-#define GB_MEMORY_MAP_LOMEM_NUM_BUCKETS (GB_MEMORY_MAP_LOMEM_SIZE/GB_MEMORY_MAP_LOMEM_BUCKET_SIZE)
-#define GB_MEMORY_MAP_HIMEM_START       (0xFE00)
-#define GB_MEMORY_MAP_HIMEM_SIZE        (0x200)
-#define GB_MEMORY_MAP_HIMEM_BUCKET_SIZE (0x1)
-#define GB_MEMORY_MAP_HIMEM_NUM_BUCKETS (GB_MEMORY_MAP_HIMEM_SIZE/GB_MEMORY_MAP_HIMEM_BUCKET_SIZE)
-
 gb_memory_map::gb_memory_map()
-    : m_lomem_readable_devices(GB_MEMORY_MAP_LOMEM_NUM_BUCKETS, nullptr),
-      m_lomem_writeable_devices(GB_MEMORY_MAP_LOMEM_NUM_BUCKETS, nullptr),
-      m_himem_readable_devices(GB_MEMORY_MAP_HIMEM_NUM_BUCKETS, nullptr),
-      m_himem_writeable_devices(GB_MEMORY_MAP_HIMEM_NUM_BUCKETS, nullptr)
+    : m_lomem_readable_devices({}), m_lomem_writeable_devices({}), m_himem_readable_devices({}), m_himem_writeable_devices({})
 {
 }
 
 gb_memory_map::~gb_memory_map() {
 }
 
-void gb_memory_map::_add_device_to_map(gb_device_map_t& device_map, const gb_memory_mapped_device_ptr& device, uint16_t start_addr, size_t end_addr, size_t bucket_size) {
+template <size_t S>
+void gb_memory_map::_add_device_to_map(gb_device_map_t<S>& device_map, const gb_memory_mapped_device_ptr& device, uint16_t start_addr, size_t end_addr, size_t bucket_size) {
     if (device == nullptr) throw std::invalid_argument("gb_memory_map::_add_device_to_map - got nullptr");
 
     for (uint64_t i = start_addr; i < end_addr; i += bucket_size) {
@@ -39,13 +27,13 @@ void gb_memory_map::add_readable_device(const gb_memory_mapped_device_ptr device
 
     // Add device to either the LOMEM or HIMEM lists or both depending on the address range
     if (start_addr < GB_MEMORY_MAP_EORAM_START) {
-        _add_device_to_map(m_lomem_readable_devices, device, start_addr, std::min<size_t>(end_addr, GB_MEMORY_MAP_EORAM_START-1), GB_MEMORY_MAP_LOMEM_BUCKET_SIZE);
+        _add_device_to_map<GB_MEMORY_MAP_LOMEM_NUM_BUCKETS>(m_lomem_readable_devices, device, start_addr, std::min<size_t>(end_addr, GB_MEMORY_MAP_EORAM_START-1), GB_MEMORY_MAP_LOMEM_BUCKET_SIZE);
         // Add device to both LOMEM and HIMEM if the span crosses both regions
         if (end_addr >= GB_MEMORY_MAP_HIMEM_START) {
-            _add_device_to_map(m_himem_readable_devices, device, 0, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
+            _add_device_to_map<GB_MEMORY_MAP_HIMEM_NUM_BUCKETS>(m_himem_readable_devices, device, 0, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
         }
     } else if (start_addr >= GB_MEMORY_MAP_HIMEM_START) {
-        _add_device_to_map(m_himem_readable_devices, device, start_addr-GB_MEMORY_MAP_HIMEM_START, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
+        _add_device_to_map<GB_MEMORY_MAP_HIMEM_NUM_BUCKETS>(m_himem_readable_devices, device, start_addr-GB_MEMORY_MAP_HIMEM_START, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
     }
 }
 
@@ -54,13 +42,13 @@ void gb_memory_map::add_writeable_device(const gb_memory_mapped_device_ptr devic
 
     // Add device to either the LOMEM or HIMEM lists or both depending on the address range
     if (start_addr < GB_MEMORY_MAP_EORAM_START) {
-        _add_device_to_map(m_lomem_writeable_devices, device, start_addr, std::min<size_t>(end_addr, GB_MEMORY_MAP_EORAM_START-1), GB_MEMORY_MAP_LOMEM_BUCKET_SIZE);
+        _add_device_to_map<GB_MEMORY_MAP_LOMEM_NUM_BUCKETS>(m_lomem_writeable_devices, device, start_addr, std::min<size_t>(end_addr, GB_MEMORY_MAP_EORAM_START-1), GB_MEMORY_MAP_LOMEM_BUCKET_SIZE);
         // Add device to both LOMEM and HIMEM if the span crosses both regions
         if (end_addr >= GB_MEMORY_MAP_HIMEM_START) {
-            _add_device_to_map(m_himem_writeable_devices, device, 0, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
+            _add_device_to_map<GB_MEMORY_MAP_HIMEM_NUM_BUCKETS>(m_himem_writeable_devices, device, 0, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
         }
     } else if (end_addr >= GB_MEMORY_MAP_HIMEM_START) {
-        _add_device_to_map(m_himem_writeable_devices, device, start_addr-GB_MEMORY_MAP_HIMEM_START, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
+        _add_device_to_map<GB_MEMORY_MAP_HIMEM_NUM_BUCKETS>(m_himem_writeable_devices, device, start_addr-GB_MEMORY_MAP_HIMEM_START, end_addr-GB_MEMORY_MAP_HIMEM_START, GB_MEMORY_MAP_HIMEM_BUCKET_SIZE);
     }
 }
 
